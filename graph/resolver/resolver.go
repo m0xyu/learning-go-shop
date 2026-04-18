b05f71934423bb31ad2cbbe3e7208dd2ca27e1fb
xaseki <xaseki@xeartsnoMacBook-Pro.local>
2026-04-19 02:58:50 +0900

    add dependencies

--- graph/resolver/resolver.go
+++ graph/resolver/resolver.go
@@ -1,8 +1,41 @@
 package resolver
 
+import (
+	"strconv"
+
+	"github.com/m0xyu/learning-go-shop/internal/services"
+)
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require
 // here.
 
-type Resolver struct{}
+type Resolver struct {
+	authService    services.AuthService
+	userService    services.UserService
+	productService services.ProductService
+	cartService    services.CartService
+	orderService   services.OrderService
+}
+
+func NewResolver(
+	authService services.AuthService,
+	userService services.UserService,
+	productService services.ProductService,
+	cartService services.CartService,
+	orderService services.OrderService,
+) *Resolver {
+	return &Resolver{
+		authService:    authService,
+		userService:    userService,
+		productService: productService,
+		cartService:    cartService,
+		orderService:   orderService,
+	}
+}
+
+func (r *Resolver) parseID(id string) (uint, error) {
+	parsed, err := strconv.ParseUint(id, 10, 32)
+	return uint(parsed), err
+}
