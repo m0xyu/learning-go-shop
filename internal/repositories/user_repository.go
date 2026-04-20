7658ea1979207b997b91eef2ac916e91956d9c87
xaseki <xaseki@xeartsnoMacBook-Pro.local>
2026-04-21 04:39:18 +0900

    add implementation for user repository

--- internal/repositories/user_repository.go
+++ internal/repositories/user_repository.go
@@ -1,6 +1,8 @@
 package repositories
 
 import (
+	"time"
+
 	"github.com/m0xyu/learning-go-shop/internal/models"
 	"gorm.io/gorm"
 )
@@ -14,34 +16,55 @@ func NewUserRepository(db *gorm.DB) *UserRepository {
 }
 
 func (r *UserRepository) GetByEmail(email string) (*models.User, error) {
-	return nil, nil
+	var user models.User
+	if err := r.db.Where("email = ?", email).First(&user).Error; err != nil {
+		return nil, err
+	}
+	return &user, nil
 }
 
 func (r *UserRepository) GetByID(id uint) (*models.User, error) {
-	return nil, nil
+	var user models.User
+	if err := r.db.First(&user, id).Error; err != nil {
+		return nil, err
+	}
+	return &user, nil
 }
+
 func (r *UserRepository) GetByEmailAndActive(email string, isActive bool) (*models.User, error) {
-	return nil, nil
+	var user models.User
+	if err := r.db.Where("email = ? AND is_active = ?", email, isActive).First(&user).Error; err != nil {
+		return nil, err
+	}
+	return &user, nil
 }
+
 func (r *UserRepository) Create(user *models.User) error {
-	return nil
+	return r.db.Create(user).Error
 }
 func (r *UserRepository) Update(user *models.User) error {
-	return nil
+	return r.db.Save(user).Error
 }
 func (r *UserRepository) Delete(id uint) error {
-	return nil
+	return r.db.Delete(&models.User{}, id).Error
 }
 
 func (r *UserRepository) CreateRefreshToken(token *models.RefreshToken) error {
-	return nil
+	return r.db.Create(token).Error
 }
+
 func (r *UserRepository) GetValidRefreshToken(token string) (*models.RefreshToken, error) {
-	return nil, nil
+	var refreshToken models.RefreshToken
+	if err := r.db.Where("token = ? AND expires_at > ?", token, time.Now()).First(&refreshToken).Error; err != nil {
+		return nil, err
+	}
+	return &refreshToken, nil
 }
+
 func (r *UserRepository) DeleteRefreshToken(token string) error {
-	return nil
+	return r.db.Where("token = ?", token).Delete(&models.RefreshToken{}).Error
 }
+
 func (r *UserRepository) DeleteRefreshTokenByID(id uint) error {
-	return nil
+	return r.db.Delete(&models.RefreshToken{}, id).Error
 }
